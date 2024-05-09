4d5fa545a2974af16f0fc50282ab19637af3152e
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-05-08 21:01:20 -0500

    Practica API y fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [data,setData] = useState(null);
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((Response)=>Response.json())
+    .then((data)=>setData(data));
+  }, []);  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Fetch a una API Json</h1>
+      <div className="card"></div>
+      <ul>
+        {data?.map((user)=>(<li key={user.id}>{user.name}</li>))}
+      </ul>
     </div>
   );
 }
